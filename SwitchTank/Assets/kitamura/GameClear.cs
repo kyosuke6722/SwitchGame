ecccc67175cf4ec9bc6d8fbd9f49f7da1dacf79a
kitamura981 <22051907@d.anabuki-college.net>
2024-04-26 10:51:30 +0900

    4/26GameUI

---
+++ SwitchTank/Assets/kitamura/GameClear.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClear : MonoBehaviour
+{
+    private static GameClear ms_instane = null;
+    private GameObject[] enemyBox;
+
+    public static void StartGameOver()
+    {
+        ms_instane.gameObject.SetActive(true);
+    }
+
+    private void Awake()
+    {
+        ms_instane = this;
+        gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        enemyBox = GameObject.FindGameObjectsWithTag("Enemy");
+
+        print("ìGÇÃêî:"+enemyBox.Length);
+
+        if(enemyBox.Length == 0 ) {
+            SceneManager.LoadScene("GameClear");
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Scene scene=SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
---
+++ SwitchTank/Assets/kitamura/GameOver.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    private static GameOver ms_instance = null;
+
+    public static bool IsGameOver()
+    {
+        return ms_instance.gameObject.activeSelf;
+    }
+    public static void StartGameOver()
+    {
+        ms_instance.gameObject.SetActive(true);
+    }
+
+    private void Awake()
+    {
+        ms_instance = this;
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+        Scene scene=SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        }
+    }
+}
