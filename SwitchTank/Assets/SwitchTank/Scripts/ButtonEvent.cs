fe7c3a90eb6cd5ef3ad935a796bc64866df1b8df
yuzasan <22051923@d.anabuki-college.net>
2024-05-15 10:52:43 +0900

    本体Scene追加

---
+++ SwitchTank/Assets/SwitchTank/Scripts/ButtonEvent.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    [SerializeField]
+    private int m_stage = 0;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnPressed()
+    {
+        SwitchScene();
+    }
+
+    public void SwitchScene()
+    {
+        switch (m_stage)
+        {
+            case 0:
+                SceneManager.LoadScene("Title");
+                break;
+            case 1:
+                SceneManager.LoadScene("NPCVS1");
+                break;
+            case 2:
+                SceneManager.LoadScene("NPCVS2");
+                break;
+            case 3:
+                SceneManager.LoadScene("NPCVS3");
+                break;
+            case 4:
+                SceneManager.LoadScene("PlayerVS");
+                break;
+            case 5:
+                SceneManager.LoadScene("GameClear");
+                break;
+            case 6:
+                SceneManager.LoadScene("GameOver");
+                break;
+        }
+        
+    }
+}
