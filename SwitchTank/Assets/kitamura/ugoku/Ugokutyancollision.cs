09c0aa471dee0dac494ff341973a77043e640a99
yuzasan <22051923@d.anabuki-college.net>
2024-05-20 09:42:13 +0900

    5/20　ソースコード修正

--- SwitchTank/Assets/kitamura/ugoku/Ugokutyancollision.cs
+++ SwitchTank/Assets/kitamura/ugoku/Ugokutyancollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collision : MonoBehaviour
+public class Ugokutyancollision : MonoBehaviour
 {
 
     public GameObject floor;
--- SwitchTank/Assets/kitamura/ugoku/ugokutyan.cs
+++ SwitchTank/Assets/kitamura/ugoku/ugokutyan.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ugokutyan : MonoBehaviour
 {
     float x = 0.01f;
     // Start is called before the first frame update
