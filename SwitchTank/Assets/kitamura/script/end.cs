dbb9558c2085e0a6afc9204490be818fb9b70721
kitamura981 <22051907@d.anabuki-college.net>
2024-06-05 15:10:44 +0900

    6/5 Title BGM

--- SwitchTank/Assets/kitamura/script/end.cs
+++ SwitchTank/Assets/kitamura/script/end.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class end : MonoBehaviour
 {
     public GameObject effectPrefab;
+    public AudioClip sound1;
+    AudioSource audioSource;
     //public GameObject GameManager;
 
-    private GameManager GameManager;
+  //  private GameManager GameManager;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "jirai")
         {
             //GameOver.StartGameOver();
-            GameManager.StageClear();
+            //GameManager.StageClear();
             Destroy(gameObject, 0.2f);
+            //audioSource.PlayOneShot(sound1);
             Destroy(collision.gameObject, 0.2f);
             GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
 
