5be4aa11b98211529e3704a31da9645c5aac730d
kitamura981 <22051907@d.anabuki-college.net>
2024-06-07 12:35:06 +0900

    6/7 pvp bgm

--- SwitchTank/Assets/SwitchTank/Scripts/Destroy.cs
+++ SwitchTank/Assets/SwitchTank/Scripts/Destroy.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
     public GameObject effectPrefab;
+    public AudioClip se;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bullet")
         {
 
             Destroy(gameObject, 0.2f);
+
             GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
-            
-           // Destroy(effect, 2.0f);
+            AudioSource.PlayClipAtPoint(se, transform.position);
+            // Destroy(effect, 2.0f);
 
         }
     }
--- SwitchTank/Assets/SwitchTank/Scripts/owari.cs
+++ SwitchTank/Assets/SwitchTank/Scripts/owari.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class owari : MonoBehaviour
 {
     public GameObject effectPrefab;
-   // public GameObject GameManager;
-    
-   // private PlayerCount PlayerCount;
+    public AudioClip se;
+    // public GameObject GameManager;
+
+    // private PlayerCount PlayerCount;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "jirai")
@@ -16,6 +17,7 @@ public class owari : MonoBehaviour
             //PlayerCount.StageClear();
             Destroy(gameObject, 0.2f);
             Destroy(collision.gameObject, 0.2f);
+            //AudioSource.PlayClipAtPoint(se, transform.position);
             GameObject effect=Instantiate(effectPrefab,transform.position,Quaternion.identity);
 
             Destroy(effect,2.0f);
