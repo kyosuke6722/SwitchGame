1fb9b3f168b0fed4b8ec120da9d660b3ab406435
kitamura981 <22051907@d.anabuki-college.net>
2024-06-12 15:11:14 +0900

    6/12

---
+++ SwitchTank/Assets/kitamura/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using nn.oe;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text CountText;
+    float countdown = 3f;
+    int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdown>=0)
+        {
+            countdown -= Time.deltaTime;
+            count = (int)countdown;
+            CountText.text = count.ToString();          
+        }
+       
+    }
+}
